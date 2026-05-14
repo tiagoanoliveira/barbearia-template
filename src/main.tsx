15a642b9e1743a37a96240da2c88c4c467e5f5bf
tiagoanoliveira <up202007448@up.pt>
2026-05-14 09:35:00 +0100

    Fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './styles/index.css'
+import './styles/globals.css'
 import { registerPWA } from './pwa-register'
 
 // Regista o SW e injeta o manifest correto ANTES do render
