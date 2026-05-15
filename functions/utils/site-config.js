8760c71489ada61fc18fdc1035ab9abc5edb551a
Tiago Oliveira <geral@tiagoanoliveira.pt>
2026-05-15 01:03:35 +0100

    Update logo path in site configuration

--- functions/utils/site-config.js
+++ functions/utils/site-config.js
@@ -32,7 +32,7 @@ export const SHOP = {
   address:      'Rua do Campo Alegre, 450, Porto',
 
   // URL do logo (relativo ao baseUrl, servido do R2 ou directamente)
-  logoPath:     '/media/images/logos/logo-512px.png',
+  logoPath:     '/icons/logo-512px.png',
 }
 
 /** URL completa do logo (usada nos emails) */
